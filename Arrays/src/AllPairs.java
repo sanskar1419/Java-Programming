b6f919780341030eecb820799797f7d3b68f0e2e
SANSKAR GUPTA <sanskarguptabaisali@gmail.com>
2023-11-15 21:59:35 +0530

    All pairs program

---
+++ Arrays/src/AllPairs.java
@@ -0,0 +1,16 @@
+public class AllPairs {
+    public static void PrintPairs(int arr[])
+    {
+        for(int i = 0 ; i<arr.length-1 ; i++)
+        {
+            for(int j = i+1 ; j<arr.length ; j++ )
+            {
+              System.out.println(arr[i] + " " + arr[j]);
+            }
+        }
+    }
+    public static void main(String[] args) {
+        int arr[] = {1,2,3,4,5,6};
+        PrintPairs(arr);
+    }
+}
